5f9edf4e502a1b57cc6bb50de3e9227cda5b9f65
VeriAnalisti-VA <verianalisti9@gmail.com>
2026-04-05 12:06:29 +0300

    Satın Alma Maliyeti

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,6 +1,7 @@
 SELECT
     date_date,
-    SUM (turnover) AS turnover
+    SUM (turnover) AS turnover,
+    SUM(purchase_cost) AS purchase_cost
 FROM `graphic-chain-473717-s9.course14.gwz_sales`
 GROUP BY date_date
 ORDER BY date_date
